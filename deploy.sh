99de6f89249b77ef313364c2b6d963f820ec1d3c
HaithamMahajna <haithammahajna@outlook.com>
2025-07-11 23:32:20 +0300

    Update deploy.sh
    delete apache

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,6 @@ sudo systemctl daemon-reload
 sudo systemctl restart nextjs.service
 sudo systemctl enable nextjs.service
 sudo systemctl status nextjs.service --no-pager
-sudo systemctl restart apache2
 if ! systemctl is-active --quiet nextjs.service; then
   echo "❌tictactoe is not running."
   sudo systemctl status nextjs.service --no-pager
